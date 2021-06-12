9cb607af2c9f0c538fee3d5972e01bf51e980e54
Krubiniki <mkrubiniki@gmail.com>
2021-06-12 18:08:54 -0300

    add home style, scheduling style

--- hairstylist/src/app/step-displayer/step-displayer-routing.module.ts
+++ hairstylist/src/app/step-displayer/step-displayer-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SchedulingComponent } from './scheduling/scheduling.component';
 
 const routes: Routes = [
   { path:'', component: HomeComponent },
   { path:'agendamento', component: SchedulingComponent },
+  { path:'login', component: LoginComponent }
 ];
 
 @NgModule({
--- hairstylist/src/app/step-displayer/step-displayer.module.ts
+++ hairstylist/src/app/step-displayer/step-displayer.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { StepDisplayerRoutingModule } from 'src/app/step-displayer/step-displayer-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SchedulingComponent } from './scheduling/scheduling.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SchedulingComponent
+    SchedulingComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
